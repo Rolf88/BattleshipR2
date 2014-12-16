a194f0deb7fc572da50b41f6d3c7e41634d345b5
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-12-16 13:29:50 +0100

    Generate a 2d-map

--- src/src/r2/Ai1.java
+++ src/src/r2/Ai1.java
@@ -5,8 +5,6 @@
  */
 package r2;
 
-
-
 import battleship.interfaces.BattleshipsPlayer;
 import battleship.interfaces.Board;
 import battleship.interfaces.Fleet;
@@ -23,38 +21,51 @@ import java.util.Random;
 public class Ai1 implements BattleshipsPlayer {
 
     private final static Random rnd = new Random();
-    private int sizeX;
-    private int sizeY;
 
     private int nextX;
     private int nextY;
-    
+
+    private int sizeX;
+    private int sizeY;
+    private int[][] map;
+
     private ArrayList<Pos> emptyfields;
-    
+
     public Ai1() {
-        
+
     }
 
     @Override
     public void placeShips(Fleet fleet, Board board) {
-        emptyfields= new ArrayList<>();
-        for(int y = 0; y < board.sizeY(); ++y){
-            for (int x = 0; x < board.sizeX(); ++x) {
-                
-                emptyfields.add(new Pos(x,y));
+        // Save the size of the board
+        this.sizeX = board.sizeX();
+        this.sizeY = board.sizeY();
+
+        // Create a map
+        this.map = new int[this.sizeX][this.sizeY];
+        for (int x = 0; x < this.sizeX; x++) {
+            for (int y = 0; y < this.sizeY; y++) {
+                this.map[x][y] = 0;
             }
         }
         
+
+        emptyfields = new ArrayList<>();
+        for (int y = 0; y < board.sizeY(); ++y) {
+            for (int x = 0; x < board.sizeX(); ++x) {
+
+                emptyfields.add(new Pos(x, y));
+            }
+        }
+
         nextX = 0;
         nextY = 0;
-        sizeX = board.sizeX();
-        sizeY = board.sizeY();        
-        
+
         for (int i = 0; i < fleet.getNumberOfShips(); ++i) {
             Ship s = fleet.getShip(i);
             boolean vertical = rnd.nextBoolean();
             Position pos;
-            
+
             if (vertical) {
                 int x = rnd.nextInt(sizeX);
                 int y = rnd.nextInt(sizeY - (s.size() - 1));
@@ -64,15 +75,12 @@ public class Ai1 implements BattleshipsPlayer {
                 int x = rnd.nextInt(sizeX - (s.size() - 1));
                 int y = rnd.nextInt(sizeY);
                 pos = new Position(x, y);
-                  
+
             }
-            
-            
-            
+
             board.placeShip(pos, s, vertical);
         }
-        }
-    
+    }
 
     @Override
     public void incoming(Position pos) {
