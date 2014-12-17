6ac2178c460a48c6576d687de3d537d0b8ec7702
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-12-17 10:13:52 +0100

    Add ship to ship placement

--- src/src/r2/domain/models/ShipPlacement.java
+++ src/src/r2/domain/models/ShipPlacement.java
@@ -1,16 +1,19 @@
 package r2.domain.models;
 
 import battleship.interfaces.Position;
+import battleship.interfaces.Ship;
 
 public class ShipPlacement {
     private final Position position;
     private final int length;
     private final boolean isVertical;
+    private final Ship ship;
 
-    public ShipPlacement(Position position, int length, boolean isVertical) {
+    public ShipPlacement(Position position, int length, boolean isVertical, Ship ship) {
         this.position = position;
         this.length = length;
         this.isVertical = isVertical;
+        this.ship = ship;
     }
 
     public Position getPosition() {
@@ -21,7 +24,11 @@ public class ShipPlacement {
         return length;
     }
 
-    public boolean isIsVertical() {
+    public boolean isVertical() {
         return isVertical;
+    }
+    
+    public Ship getShip(){
+        return this.ship;
     }
 }
