1e420f569612259278441a5646e29c53f92c5462
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-12-18 13:42:21 +0100

    Remove mark when not a valid shipplacement

--- src/src/r2/shipPlacers/BaseShipPlacer.java
+++ src/src/r2/shipPlacers/BaseShipPlacer.java
@@ -16,12 +16,14 @@ public abstract class BaseShipPlacer implements IShipPlacer {
 
     protected boolean isValidPlacement(int sizeX, int sizeY, int[][] shipMap, Position position, int length, boolean isVertical) {
         if (isVertical) {
-
             if (position.y + length > sizeY) {
                 return false;
             }
             for (int y = position.y; y < position.y + length; y++) {
                 if (shipMap[position.x][y] != 0) {
+                    for (int ry = y - 1; ry >= position.y; ry--) {
+                        shipMap[position.x][ry] = 0;
+                    }
                     return false;
                 }
                 shipMap[position.x][y] = 1;
@@ -33,6 +35,9 @@ public abstract class BaseShipPlacer implements IShipPlacer {
         }
         for (int x = position.x; x < position.x + length; x++) {
             if (shipMap[x][position.y] != 0) {
+                for (int rx = x - 1; rx >= position.x; rx--) {
+                    shipMap[rx][position.y] = 0;
+                }
                 return false;
             }
             shipMap[x][position.y] = 1;
